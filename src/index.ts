b3ac37fb31e531dfc54f54d49bdd8107eafd89a6
hoge1e3 <hoge1e3@gmail.com>
2025-04-10 17:59:29 +0900

    stop/ mml

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,76 @@ type ADSR = {
     release: number; // time in seconds to fade out
 };
 type Waveform = 'sine' | 'square' | 'sawtooth' | 'triangle';
-const audioCtx = new AudioContext();
-export function createNode(time:number, duration:number, freq:number, vol:number, waveform:Waveform,  envelope:ADSR) {
-    const oscillator = audioCtx.createOscillator();
-    const gainNode = audioCtx.createGain();
-
-    oscillator.type = waveform; // type of wave
-    oscillator.frequency.setValueAtTime(freq, time); // frequency in hertz
-
-    // Apply ADSR envelope to the gain node
-    const attackEnd = time + envelope.attack;
-    const decayEnd = attackEnd + envelope.decay;
-    gainNode.gain.setValueAtTime(0, time); // Start at 0 volume
-    gainNode.gain.linearRampToValueAtTime(vol, attackEnd); // Attack phase
-    gainNode.gain.linearRampToValueAtTime(vol * envelope.sustain, decayEnd); // Decay phase
-    gainNode.gain.setValueAtTime(vol * envelope.sustain, decayEnd); // Sustain phase
-    gainNode.gain.linearRampToValueAtTime(0, time + duration); // Release phase
-
-    oscillator.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-
-    oscillator.start(time);
-    oscillator.stop(time + duration);
+export interface Source {
+    duration: number;
+    play(ctx:AudioContext, start?:number, dest?: AudioDestinationNode ):Playback;
+}
+export function joinSource(...sources:Source[]):Source {
+    const duration=sources.reduce((prev, cur)=>prev+cur.duration ,0);
+    return {
+        duration,
+        play(ctx:AudioContext, start:number=ctx.currentTime, dest=ctx.destination ) {
+            const end=start+duration;
+            const playbacks=sources.reduce((pbs, src)=>[
+                ...pbs, 
+                src.play(ctx, pbs[pbs.length-1]?.end||start, dest)
+            ],  [] as Playback[]);
+            return {
+                ctx ,dest, start, end,
+                stop() {
+                    for (let p of playbacks) p.stop();
+                },
+                join(src:Source):Playback {
+                    return src.play(ctx,end,dest);
+                }
+            }
+        }
+    }
+}
+export interface Playback {
+    ctx:AudioContext;
+    dest: AudioDestinationNode;
+    stop():void;
+    start:number;
+    end:number; // end time may be decided later.
+    //join(src:Source):Playback;
+}
+export function createNote(duration:number, freq:number, vol:number, waveform:Waveform,  envelope:ADSR):Source {
+    return {
+        duration,
+        play(ctx:AudioContext, start:number=ctx.currentTime, dest=ctx.destination ) {
+            const oscillator = ctx.createOscillator();
+            const gainNode = ctx.createGain();
+        
+            oscillator.type = waveform; // type of wave
+            oscillator.frequency.setValueAtTime(freq, start); // frequency in hertz
+        
+            // Apply ADSR envelope to the gain node
+            const attackEnd = start + envelope.attack;
+            const decayEnd = attackEnd + envelope.decay;
+            gainNode.gain.setValueAtTime(0, start); // Start at 0 volume
+            gainNode.gain.linearRampToValueAtTime(vol, attackEnd); // Attack phase
+            gainNode.gain.linearRampToValueAtTime(vol * envelope.sustain, decayEnd); // Decay phase
+            gainNode.gain.setValueAtTime(vol * envelope.sustain, decayEnd); // Sustain phase
+            gainNode.gain.linearRampToValueAtTime(0, start + duration); // Release phase
+        
+            oscillator.connect(gainNode);
+            gainNode.connect(dest);
+            const end=start + duration
+            oscillator.start(start);
+            oscillator.stop(end);
+            //const endp=Promise.resolve(end);
+            return {
+                gainNode, oscillator,
+                ctx ,dest, start, end,
+                stop() {
+                    //console.log("Discon");
+                    gainNode.disconnect()
+                },
+                /*join(src:Source):Playback {
+                    return src.play(ctx,end,dest);
+                }*/
+            }
+        }
+    };
 }
---
+++ src/mml.ts
@@ -0,0 +1,34 @@
+import { Source } from ".";
+
+export type LieteralSet={
+    scales:string[],
+    longSyllable: string, 
+    rhysms?: Map<string, Source>;
+}
+export const standardLiteralSet:LieteralSet={
+    scales: ["a","b","c","d","e","f","g"],
+    longSyllable: "^", 
+    
+};
+export const japaneseLiteralSet:LieteralSet={
+    scales: ["ラ","シ","ド","レ","ミ","ファ","ソ"],
+    longSyllable: "ー", 
+};
+
+export interface NoteLength {
+    n: number;   
+    d: number; //   n/d
+}
+export interface Note {
+    scale: number; //0-95
+    length: NoteLength;
+}
+export type Melody=Note[];
+export class Parser {
+    constructor(public literals: LieteralSet) {
+
+    }
+    parse(mml:string):Melody {
+        
+    }
+}
